353673b88acc0be966c3a857aa52a1e32d1c0e6e
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2018-02-08 20:20:17 -0800

    Added system codes
    This adds enums describing the sub systems and their status codes.
    
    It also adds functions that parse and generate the system messages.

---
+++ src/tfr_utilities/src/status_code.cpp
@@ -0,0 +1,129 @@
+#include "status_code.h"
+
+//Defines the mask to use to identify a Sub System given a StatusCode
+enum { SystemMask = 0b1111111100000000 };
+
+/**
+ * Sub System specific getters for system messages.
+ *
+ * Any new status codes should have their associated messaged added into these
+ * functions.
+ */
+std::string parseExecCode(StatusCode code, float data);
+std::string parseLocCode(StatusCode code, float data);
+std::string parseNavCode(StatusCode code, float data);
+std::string parseMineCode(StatusCode code, float data);
+std::string parseDumpCode(StatusCode code, float data);
+
+
+std::string getStatusMessage(StatusCode code, float data)
+{
+    //switch to find the sub system and use associated code parser
+    switch (static_cast<SubSystem>(SystemMask & static_cast<uint16_t>(code)))
+    {
+        case SubSystem::EXC:
+        {
+            return parseExecCode(code, data);
+        }
+        case SubSystem::LOC:
+        {
+            return parseLocCode(code, data);
+        }
+        case SubSystem::NAV:
+        {
+            return parseNavCode(code, data);
+        }
+        case SubSystem::MIN:
+        {
+            return parseMineCode(code, data);
+        }
+        case SubSystem::DMP:
+        {
+            return parseDumpCode(code, data);
+        }
+        default:
+        {
+            return "Warning: Unknown sub system identifier received.";
+        }
+    }
+}
+
+//Parser for all status codes in the Executive sub system
+std::string parseExecCode(StatusCode code, float data)
+{
+    switch(code)
+    {
+        case StatusCode::EXC_OK:
+        {
+            return "Executive System OK";
+        }
+        default:
+        {
+            return "Warning: Unknown status code for Executive received.";
+        }
+    }
+}
+
+//Parser for all status codes in the Localization sub system
+std::string parseLocCode(StatusCode code, float data)
+{
+    switch(code)
+    {
+        case StatusCode::LOC_OK:
+        {
+            return "Localization System OK";
+        }
+        default:
+        {
+            return "Warning: Unknown status code for Localization received.";
+        }
+    }
+}
+
+//Parser for all status codes in the Navigation sub system
+std::string parseNavCode(StatusCode code, float data)
+{
+    switch(code)
+    {
+        case StatusCode::NAV_OK:
+        {
+            return "Navigation System OK";
+        }
+        default:
+        {
+            return "Warning: Unknown status code for Navigation received.";
+        }
+    }
+}
+
+//Parser for all status codes in the Mining sub system
+std::string parseMineCode(StatusCode code, float data)
+{
+    switch(code)
+    {
+        case StatusCode::MIN_OK:
+        {
+            return "Mining System OK";
+        }
+        default:
+        {
+            return "Warning: Unknown status code for Mining received.";
+        }
+    }
+}
+
+//Parser for all status codes in the Dumping sub system
+std::string parseDumpCode(StatusCode code, float data)
+{
+    switch(code)
+    {
+        case StatusCode::DMP_OK:
+        {
+            return "Dumping System OK";
+        }
+        default:
+        {
+            return "Warning: Unknown status code for Dumping received.";
+        }
+    }
+}

9be0359de0226df3b3bb634ecee73b50505d98f2
collinsc <collinsconway@gmail.com>
2018-02-26 23:41:11 -0800

    added in gpio capability and cleaned up jetson build

--- src/tfr_control/src/pwm_bridge.cpp
+++ src/tfr_control/src/pwm_bridge.cpp
@@ -6,64 +6,103 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <JHPWMPCA9685.h>
-
-/**
- *  Convenience function for conceptualizing output.
- *  Takes in a number from -100 -> 100 specifying the desired output strength,
- *  where -100 = maximum speed reverse, 0 = neutral, and 100 is full speed
- *  ahead.
- *  Actual PWM outs for our cim
- *  340 = full reverse
- *  520 = stop
- *  700 = full ahead
- *  not sure our production cim's will be at all similar, so this is pretty
- *  temporary
- * */
-int map(int x)
+extern "C"
 {
-    //checks input
-    if (x < -100 || x > 100)
-        return 0;
-    double magnitude = x * 180.0/100.0;
-    long rounded = std::lround(magnitude);
-    return 520 + static_cast<int>(rounded);
+    #include <gpio.h>
 }
+
+class PWMInterface
+{
+    public:
+        enum class Address
+        {
+            LEFT_TREAD = 0
+        };
+
+        PWMInterface()
+        {
+            gpioExport(gpio389);
+            gpioSetDirection(gpio389, outputPin);
+            enablePWM(false);
+            ROS_INFO("here");
+            int err = pca9685->openPCA9685();
+            ROS_INFO("here %d", err);
+            if (err < 0)
+            {
+                ROS_WARN("Error: %d", pca9685->error);
+            }    
+            ROS_INFO("PCA9685 Device Address: 0x%02X\n",pca9685->kI2CAddress);
+            pca9685->setAllPWM(0,0) ;
+            pca9685->reset() ;
+            pca9685->setPWMFrequency(80) ;
+        };
+
+        ~PWMInterface()
+        {
+            gpioUnexport(gpio389);
+            gpioClose(gpio389);
+        }
+
+        void set(Address address, double value )
+        {
+            if (value < -1 || value > 1)
+            {
+                ROS_WARN("Incorrect Motor Value: %f", value);
+                return;
+            }
+            pca9685->setPWM(static_cast<int>(address),0,map(value));
+        }
+
+        void enablePWM(bool value)
+        {
+            gpioSetValue(gpio389, (value)? off : on);
+        }
+
+    private:
+        /**
+         *  Convenience function for conceptualizing output.
+         *  Takes in a number from -1 -> 1 specifying the desired output strength,
+         *  where -1 = maximum speed reverse, 0 = neutral, and 1 is full speed
+         *  ahead.
+         *  Actual PWM outs for our cim
+         *  340 = full reverse
+         *  520 = stop
+         *  700 = full ahead
+         *  not sure our production cim's will be at all similar, so this is pretty
+         *  temporary
+         * */
+        int map(double x)
+        {
+            //checks input
+            if (x < -100 || x > 100)
+                return 0;
+            double magnitude = x * 180.0;
+            long rounded = std::lround(magnitude);
+            return 520 + static_cast<int>(rounded);
+        }
+
+        std::unique_ptr<PCA9685> pca9685{new PCA9685()};
+};
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "blink");
     ros::NodeHandle n;
     ROS_INFO("Starting node");
 
-    //min and max values for led, max = 4096
-    int min = 0;
-    int max = 666;
+    PWMInterface interface{};
+    interface.enablePWM(true);
 
-
-    std::unique_ptr<PCA9685> pca9685 (new PCA9685());
-
-    int err = pca9685->openPCA9685();
-
-    if (err < 0)
+    bool io = false;
+    for (double j = -1; j <= 1; j += 0.01, io = !io)
     {
-        ROS_WARN("Error: %d", pca9685->error);
-    } 
-    else 
-    {
-        ROS_INFO("PCA9685 Device Address: 0x%02X\n",pca9685->kI2CAddress);
-        pca9685->setAllPWM(0,0) ;
-        pca9685->reset() ;
-        //max 999, 
-        pca9685->setPWMFrequency(80) ;
-        // 27 is the ESC key
-        for (int j = -100; j <= 100; j +=10)
-        {
-            ROS_INFO("cycle %d", j);
-            pca9685->setPWM(0,0,map(j));
-            sleep(1);
-        }
-        pca9685->setPWM(0,0,map(0));
+        ROS_INFO("cycle %f", j);
+        interface.set(PWMInterface::Address::LEFT_TREAD, j);
+        sleep(2);
+        interface.enablePWM(io);
     }
+    interface.set(PWMInterface::Address::LEFT_TREAD, 0);
     return 0;
 }
-
 

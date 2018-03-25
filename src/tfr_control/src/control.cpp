275c81938fd33a2ea05acd9e02c0a345c03fd3aa
Collin <collinsconway@gmail.com>
2018-03-24 20:54:45 -0700

    refactored control to class to allow for new service

--- src/tfr_control/src/control.cpp
+++ src/tfr_control/src/control.cpp
@@ -10,45 +10,120 @@
  * PARAMETERS:
  *  ~rate: in hz how fast we want to run the control loop (double, default:10)
  * SERVICES:
- *  /toggle_motors - uses the empty service, needs to be explicitly turned on to
- *  work
+ *  /toggle_motors - uses the empty service, needs to be explicitly turned on to work
+ *  
  */
 #include <ros/ros.h>
-#include <tfr_msgs/EmptySrv.h>
+#include <std_srvs/SetBool.h>
 #include <urdf/model.h>
 #include <sstream>
 #include <controller_manager/controller_manager.h>
 #include "robot_interface.h"
 #include "bin_control_server.h"
 
+
+
+// ADAM'S TEST CODE
 // TODO adam delete this test code when ready
 // Whether we're running on hardware or using fake values
 const bool use_fake_values = true;
-class StopManager
+// If we're faking the inputs, we need to know the model constraints on
+// the arm: load them here.
+// If not, just use zeroes, the limits don't matter. TEST code
+double lower_limits[tfr_control::RobotInterface::JOINT_COUNT] = {};
+double upper_limits[tfr_control::RobotInterface::JOINT_COUNT] = {};
+
+//test code
+void initializeTestCode(ros::NodeHandle& n)
+{
+    // Get the model description 
+    std::string desc;
+    n.param<std::string>("robot_description", desc, "");
+
+    if (desc.length() == 0) 
+    {
+        ROS_WARN("robot_description is empty and controller_launcher is using fake values, quitting.");
+    }
+
+    urdf::Model model;
+    if (!model.initString(desc)) 
+    {
+        ROS_WARN("Couldn't load robot_description.");
+    }
+
+    ROS_INFO("Model loaded successfully, loading joint limits.");
+    lower_limits[static_cast<int>(tfr_control::Joint::BIN)] 
+        = model.getJoint("bin_joint")->limits->lower;
+    upper_limits[static_cast<int>(tfr_control::Joint::BIN)] 
+        = model.getJoint("bin_joint")->limits->upper;
+    lower_limits[static_cast<int>(tfr_control::Joint::LOWER_ARM)] 
+        = model.getJoint("lower_arm_joint")->limits->lower;
+    upper_limits[static_cast<int>(tfr_control::Joint::LOWER_ARM)] 
+        = model.getJoint("lower_arm_joint")->limits->upper;
+    lower_limits[static_cast<int>(tfr_control::Joint::UPPER_ARM)] 
+        = model.getJoint("upper_arm_joint")->limits->lower;
+    upper_limits[static_cast<int>(tfr_control::Joint::UPPER_ARM)] 
+        = model.getJoint("upper_arm_joint")->limits->upper;
+    lower_limits[static_cast<int>(tfr_control::Joint::SCOOP)] 
+        = model.getJoint("scoop_joint")->limits->lower;
+    upper_limits[static_cast<int>(tfr_control::Joint::SCOOP)] 
+        = model.getJoint("scoop_joint")->limits->upper;
+}
+//END TEST CODE
+
+
+class Control
 {
     public:
-        StopManager(ros::NodeHandle &n):
-            eStop{n.advertiseService("toggle_motors", &StopManager::toggle,this)},
-            enabled{false} {}
-        ~StopManager() = default;
-        StopManager(const StopManager&) = delete;
-        StopManager& operator=(const StopManager&) = delete;
-        StopManager(StopManager&&) = delete;
-        StopManager& operator=(StopManager&&) = delete;
 
-        bool isEnabled() { return enabled; }
-
-    private:
-        bool toggle(tfr_msgs::EmptySrv::Request& request,
-                tfr_msgs::EmptySrv::Response& response)
+        Control(ros::NodeHandle &n, const double& rate):
+            robot_interface{n, use_fake_values, lower_limits, upper_limits},
+            controller_interface{&robot_interface},
+            eStop{n.advertiseService("toggle_motors", &Control::toggle,this)},
+            cycle{1/rate},
+            enabled{false}
+        {}
+        
+        /*
+         * performs one iteration of the control loop
+         * */
+        void execute()
         {
-            enabled = !enabled;
-            return true;
+            //update from hardware
+            robot_interface.read();
+            //update controllers
+            controller_interface.update(ros::Time::now(), cycle);
+            if (!enabled)
+                robot_interface.clearCommands();
+            //update hardware from controllers
+            robot_interface.write();
+            cycle.sleep();
         }
+    private:
+        //the hardware layer
+        tfr_control::RobotInterface robot_interface;
+
+        //the controller layer
+        controller_manager::ControllerManager controller_interface;
+
+        //emergency stop
         ros::ServiceServer eStop;
+
+        //how fast to spin
+        ros::Duration cycle;
+
+        //if our motors are enabled
         bool enabled;
 
-
+        /*
+         * Toggles the emergency stop on and off
+         * */
+        bool toggle(std_srvs::SetBool::Request& request,
+                std_srvs::SetBool::Response& response)
+        {
+            enabled = request.data;
+            return true;
+        }
 
 };
 
@@ -60,79 +135,21 @@ int main(int argc, char **argv)
     double rate;
     ros::param::param<double>("~rate", rate, 10.0);
 
+    //test code
+    if (use_fake_values)
+        initializeTestCode(n);
+
     // Start a spinner for ros node in the background, seperate from this thread
     // that manages the control loop
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    // If we're faking the inputs, we need to know the model constraints on
-    // the arm: load them here.
-    // If not, just use zeroes, the limits don't matter. TODO delete
-    double lower_limits[tfr_control::RobotInterface::JOINT_COUNT] = {};
-    double upper_limits[tfr_control::RobotInterface::JOINT_COUNT] = {};
+    Control control{n, rate};
 
-    //TODO delete
-    if (use_fake_values) 
-    {
-        // Get the model description 
-        std::string desc;
-        n.param<std::string>("robot_description", desc, "");
-    
-        if (desc.length() == 0) 
-        {
-            ROS_ERROR("robot_description is empty and controller_launcher is using fake values, quitting.");
-            return -1;
-        }
-
-        urdf::Model model;
-        if (!model.initString(desc)) 
-        {
-            ROS_ERROR("Couldn't load robot_description, quitting.");
-            return -1;
-        }
-
-        ROS_INFO("Model loaded successfully, loading joint limits.");
-        lower_limits[static_cast<int>(tfr_control::Joint::BIN)] 
-            = model.getJoint("bin_joint")->limits->lower;
-        upper_limits[static_cast<int>(tfr_control::Joint::BIN)] 
-            = model.getJoint("bin_joint")->limits->upper;
-        lower_limits[static_cast<int>(tfr_control::Joint::LOWER_ARM)] 
-            = model.getJoint("lower_arm_joint")->limits->lower;
-        upper_limits[static_cast<int>(tfr_control::Joint::LOWER_ARM)] 
-            = model.getJoint("lower_arm_joint")->limits->upper;
-        lower_limits[static_cast<int>(tfr_control::Joint::UPPER_ARM)] 
-            = model.getJoint("upper_arm_joint")->limits->lower;
-        upper_limits[static_cast<int>(tfr_control::Joint::UPPER_ARM)] 
-            = model.getJoint("upper_arm_joint")->limits->upper;
-        lower_limits[static_cast<int>(tfr_control::Joint::SCOOP)] 
-            = model.getJoint("scoop_joint")->limits->lower;
-        upper_limits[static_cast<int>(tfr_control::Joint::SCOOP)] 
-            = model.getJoint("scoop_joint")->limits->upper;
-    }
-
-    //the hardware layer
-    tfr_control::RobotInterface robot_interface{n, use_fake_values, lower_limits, upper_limits};
-
-    //the controller layer
-    controller_manager::ControllerManager controller_interface(&robot_interface);
-
-    StopManager stopManager{n};
-
-    // Store the time of the last update to feed to ControllerManager
-    ros::Time then = ros::Time::now();
-
-    ros::Duration cycle(1/rate);
     while (ros::ok())
     {
-        //update from hardware
-        robot_interface.read();
-        //update controllers
-        controller_interface.update(ros::Time::now(), cycle);
-        if (!stopManager.isEnabled())
-            robot_interface.clearCommands();
-        //update hardware from controllers
-        robot_interface.write();
-        cycle.sleep();
+        ros::spinOnce();
+        control.execute();
     }
     return 0;
 }

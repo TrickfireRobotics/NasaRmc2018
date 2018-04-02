a08611676e4bc0eddf20caf13bbd443fcaa6f780
collinsc <collinsconway@gmail.com>
2018-04-01 20:16:14 -0700

    fixed drivebase publisher

--- src/tfr_sensor/src/drivebase_odom_publisher.cpp
+++ src/tfr_sensor/src/drivebase_odom_publisher.cpp
@@ -10,6 +10,7 @@
  *   - ~child_frame: the frame of the robot (string, default: "base_footprint")
  *   - ~wheel_span: the separation of the treads of the robot. (double,
  *   default)
+ *   - ~rate: how quickly to publish hz. (double, default 10)
  * Subscribed topics:
  *   - /arduino :(tfr_msgs/ArduinoReading) The most current information coming
  *   in from the sensors.
@@ -18,7 +19,8 @@
  *   base_footprint tracked by tread motion.
  * */
 #include <ros/ros.h>
-#include <tfr_msgs/ArduinoReading.h>
+#include <tfr_msgs/ArduinoAReading.h>
+#include <tfr_msgs/ArduinoBReading.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_datatypes.h>
 
@@ -36,9 +38,9 @@ class DrivebaseOdometryPublisher
             y{},
             angle{}
         {
-            arduino_subscriber = 
-                n.subscribe("/arduino", 5, &DrivebaseOdometryPublisher::processOdometry, this);
-            odometry_publisher = n.advertise<nav_msgs::Odometry>("/drivebase_odometry", 5);
+            arduino_a = n.subscribe("/arduino_a", 15, &DrivebaseOdometryPublisher::readArduinoA, this);
+            arduino_b = n.subscribe("/arduino_b", 15, &DrivebaseOdometryPublisher::readArduinoB, this);
+            odometry_publisher = n.advertise<nav_msgs::Odometry>("/drivebase_odometry", 15);
         }
 
         ~DrivebaseOdometryPublisher() = default;
@@ -46,23 +48,21 @@ class DrivebaseOdometryPublisher
         DrivebaseOdometryPublisher& operator=(const DrivebaseOdometryPublisher&) = delete;
         DrivebaseOdometryPublisher(DrivebaseOdometryPublisher&&) = delete;
         DrivebaseOdometryPublisher& operator=(DrivebaseOdometryPublisher&) = delete;
-    private:
-        ros::Subscriber arduino_subscriber; //the encoder data sub
-        ros::Publisher odometry_publisher; //the pub for our processed data
-        const std::string& parent_frame; //the parent frame of the robot
-        const std::string& child_frame; //the child frame of the robot
-        const double& wheel_span;
-        double x; //the x coordinate of the robot (meters)
-        double y; //the y coordinate of the robot (meters)
-        double angle; //angle of rotation around the z axis (radians)
-        ros::Time t_0;
 
         /*
          * Main business logic for the node, takes in readings from the arduino,
          * and publishes them across the network.
          * */
-        void processOdometry(const tfr_msgs::ArduinoReading &reading)
+        void processOdometry()
         {
+        //Grab the neccessary data
+        tfr_msgs::ArduinoAReading reading_a;
+        tfr_msgs::ArduinoBReading reading_b;
+        if (latest_arduino_a != nullptr)
+            reading_a = *latest_arduino_a;
+        if (latest_arduino_b != nullptr)
+            reading_b = *latest_arduino_b;
+
             //first we process the data
             auto t_1 = ros::Time::now();
             double d_t = (t_1 - t_0).toSec();
@@ -76,8 +76,8 @@ class DrivebaseOdometryPublisher
 
             //message gives us velocity in meters/second from each individual
             //tread
-            double v_l = reading.tread_left_vel;
-            double v_r = reading.tread_right_vel;
+            double v_l = reading_a.tread_left_vel;
+            double v_r = reading_b.tread_right_vel;
 
             //basic differential kinematics to get combined velocities
             double v_ang = (v_r-v_l)/wheel_span;
@@ -130,6 +130,35 @@ class DrivebaseOdometryPublisher
             //finally send it across the network
             odometry_publisher.publish(msg);
         }
+
+
+    private:
+        ros::Subscriber arduino_a; //the encoder data sub
+        ros::Subscriber arduino_b; //the encoder data sub
+        tfr_msgs::ArduinoAReadingConstPtr latest_arduino_a;
+        tfr_msgs::ArduinoBReadingConstPtr latest_arduino_b;
+        ros::Publisher odometry_publisher; //the pub for our processed data
+        const std::string& parent_frame; //the parent frame of the robot
+        const std::string& child_frame; //the child frame of the robot
+        const double& wheel_span;
+        double x; //the x coordinate of the robot (meters)
+        double y; //the y coordinate of the robot (meters)
+        double angle; //angle of rotation around the z axis (radians)
+        ros::Time t_0;
+
+        //callback for publisher
+        void readArduinoA(const tfr_msgs::ArduinoAReadingConstPtr &msg)
+        {
+            latest_arduino_a = msg;
+        }
+
+        //callback for publisher
+        void readArduinoB(const tfr_msgs::ArduinoBReadingConstPtr &msg)
+        {
+            latest_arduino_b = msg;
+        }
+ 
+
 };
 
 int main(int argc, char **argv)
@@ -141,7 +170,15 @@ int main(int argc, char **argv)
     ros::param::param<std::string>("~parent_frame", parent_frame, "odom");
     ros::param::param<std::string>("~child_frame", child_frame, "odom");
     ros::param::param<double>("~wheel_span", wheel_span, 0.0);
+    ros::param::param<double>("~rate", wheel_span, 10.0);
     DrivebaseOdometryPublisher publisher{n, parent_frame, child_frame, wheel_span};
-    ros::spin();
+    ros::Rate rate(rate);
+    while(ros::ok())
+    {
+        publisher.processOdometry();
+        ros::spinOnce();
+        rate.sleep();
+
+    }
     return 0;
 }

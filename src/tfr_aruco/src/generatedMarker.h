b8bfb38b569e7bd206545cdd24be8dafba782b89
LinuxKid21 <linuxkid21@gmail.com>
2018-04-06 11:49:30 -0700

    actually generate file...

--- src/tfr_aruco/src/generatedMarker.h
+++ src/tfr_aruco/src/generatedMarker.h
@@ -4,220 +4,220 @@ void setBoardData(std::vector<std::vector<cv::Point3f> > &boardCorners, std::vec
 	std::vector<cv::Point3f> markerPoints;
 	markerPoints = 
 	{
-	cv::Point3f(0.02, 0.02, 0),
-	cv::Point3f(0.255, 0.02, 0),
-	cv::Point3f(0.02, 0.255, 0),
-	cv::Point3f(0.255, 0.255, 0)
+	cv::Point3f(-0.73, 0.02, 0),
+	cv::Point3f(-0.495, 0.02, 0),
+	cv::Point3f(-0.495, 0.255, 0),
+	cv::Point3f(-0.73, 0.255, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(1);
 
 	markerPoints = 
 	{
-	cv::Point3f(1.245, 0.02, 0),
-	cv::Point3f(1.48, 0.02, 0),
-	cv::Point3f(1.245, 0.255, 0),
-	cv::Point3f(1.48, 0.255, 0)
+	cv::Point3f(0.495, 0.02, 0),
+	cv::Point3f(0.73, 0.02, 0),
+	cv::Point3f(0.73, 0.255, 0),
+	cv::Point3f(0.495, 0.255, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(2);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.283, 0.178, 0),
-	cv::Point3f(0.335, 0.178, 0),
-	cv::Point3f(0.283, 0.23, 0),
-	cv::Point3f(0.335, 0.23, 0)
+	cv::Point3f(-0.467, 0.178, 0),
+	cv::Point3f(-0.415, 0.178, 0),
+	cv::Point3f(-0.415, 0.23, 0),
+	cv::Point3f(-0.467, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(3);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.351, 0.178, 0),
-	cv::Point3f(0.403, 0.178, 0),
-	cv::Point3f(0.351, 0.23, 0),
-	cv::Point3f(0.403, 0.23, 0)
+	cv::Point3f(-0.399, 0.178, 0),
+	cv::Point3f(-0.347, 0.178, 0),
+	cv::Point3f(-0.347, 0.23, 0),
+	cv::Point3f(-0.399, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(4);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.419, 0.178, 0),
-	cv::Point3f(0.471, 0.178, 0),
-	cv::Point3f(0.419, 0.23, 0),
-	cv::Point3f(0.471, 0.23, 0)
+	cv::Point3f(-0.331, 0.178, 0),
+	cv::Point3f(-0.279, 0.178, 0),
+	cv::Point3f(-0.279, 0.23, 0),
+	cv::Point3f(-0.331, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(5);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.487, 0.178, 0),
-	cv::Point3f(0.539, 0.178, 0),
-	cv::Point3f(0.487, 0.23, 0),
-	cv::Point3f(0.539, 0.23, 0)
+	cv::Point3f(-0.263, 0.178, 0),
+	cv::Point3f(-0.211, 0.178, 0),
+	cv::Point3f(-0.211, 0.23, 0),
+	cv::Point3f(-0.263, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(6);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.555, 0.178, 0),
-	cv::Point3f(0.607, 0.178, 0),
-	cv::Point3f(0.555, 0.23, 0),
-	cv::Point3f(0.607, 0.23, 0)
+	cv::Point3f(-0.195, 0.178, 0),
+	cv::Point3f(-0.143, 0.178, 0),
+	cv::Point3f(-0.143, 0.23, 0),
+	cv::Point3f(-0.195, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(7);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.623, 0.178, 0),
-	cv::Point3f(0.675, 0.178, 0),
-	cv::Point3f(0.623, 0.23, 0),
-	cv::Point3f(0.675, 0.23, 0)
+	cv::Point3f(-0.127, 0.178, 0),
+	cv::Point3f(-0.075, 0.178, 0),
+	cv::Point3f(-0.075, 0.23, 0),
+	cv::Point3f(-0.127, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(8);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.691, 0.178, 0),
-	cv::Point3f(0.743, 0.178, 0),
-	cv::Point3f(0.691, 0.23, 0),
-	cv::Point3f(0.743, 0.23, 0)
+	cv::Point3f(-0.059, 0.178, 0),
+	cv::Point3f(-0.007, 0.178, 0),
+	cv::Point3f(-0.007, 0.23, 0),
+	cv::Point3f(-0.059, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(9);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.759, 0.178, 0),
-	cv::Point3f(0.811, 0.178, 0),
-	cv::Point3f(0.759, 0.23, 0),
-	cv::Point3f(0.811, 0.23, 0)
+	cv::Point3f(0.009, 0.178, 0),
+	cv::Point3f(0.061, 0.178, 0),
+	cv::Point3f(0.061, 0.23, 0),
+	cv::Point3f(0.009, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(10);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.827, 0.178, 0),
-	cv::Point3f(0.879, 0.178, 0),
-	cv::Point3f(0.827, 0.23, 0),
-	cv::Point3f(0.879, 0.23, 0)
+	cv::Point3f(0.077, 0.178, 0),
+	cv::Point3f(0.129, 0.178, 0),
+	cv::Point3f(0.129, 0.23, 0),
+	cv::Point3f(0.077, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(11);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.895, 0.178, 0),
-	cv::Point3f(0.947, 0.178, 0),
-	cv::Point3f(0.895, 0.23, 0),
-	cv::Point3f(0.947, 0.23, 0)
+	cv::Point3f(0.145, 0.178, 0),
+	cv::Point3f(0.197, 0.178, 0),
+	cv::Point3f(0.197, 0.23, 0),
+	cv::Point3f(0.145, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(12);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.963, 0.178, 0),
-	cv::Point3f(1.015, 0.178, 0),
-	cv::Point3f(0.963, 0.23, 0),
-	cv::Point3f(1.015, 0.23, 0)
+	cv::Point3f(0.213, 0.178, 0),
+	cv::Point3f(0.265, 0.178, 0),
+	cv::Point3f(0.265, 0.23, 0),
+	cv::Point3f(0.213, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(13);
 
 	markerPoints = 
 	{
-	cv::Point3f(1.031, 0.178, 0),
-	cv::Point3f(1.083, 0.178, 0),
-	cv::Point3f(1.031, 0.23, 0),
-	cv::Point3f(1.083, 0.23, 0)
+	cv::Point3f(0.281, 0.178, 0),
+	cv::Point3f(0.333, 0.178, 0),
+	cv::Point3f(0.333, 0.23, 0),
+	cv::Point3f(0.281, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(14);
 
 	markerPoints = 
 	{
-	cv::Point3f(1.099, 0.178, 0),
-	cv::Point3f(1.151, 0.178, 0),
-	cv::Point3f(1.099, 0.23, 0),
-	cv::Point3f(1.151, 0.23, 0)
+	cv::Point3f(0.349, 0.178, 0),
+	cv::Point3f(0.401, 0.178, 0),
+	cv::Point3f(0.401, 0.23, 0),
+	cv::Point3f(0.349, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(15);
 
 	markerPoints = 
 	{
-	cv::Point3f(1.167, 0.178, 0),
-	cv::Point3f(1.219, 0.178, 0),
-	cv::Point3f(1.167, 0.23, 0),
-	cv::Point3f(1.219, 0.23, 0)
+	cv::Point3f(0.417, 0.178, 0),
+	cv::Point3f(0.469, 0.178, 0),
+	cv::Point3f(0.469, 0.23, 0),
+	cv::Point3f(0.417, 0.23, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(16);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.29, 0.015, 0),
-	cv::Point3f(0.415, 0.015, 0),
-	cv::Point3f(0.29, 0.14, 0),
-	cv::Point3f(0.415, 0.14, 0)
+	cv::Point3f(-0.46, 0.015, 0),
+	cv::Point3f(-0.335, 0.015, 0),
+	cv::Point3f(-0.335, 0.14, 0),
+	cv::Point3f(-0.46, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(17);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.445, 0.015, 0),
-	cv::Point3f(0.57, 0.015, 0),
-	cv::Point3f(0.445, 0.14, 0),
-	cv::Point3f(0.57, 0.14, 0)
+	cv::Point3f(-0.305, 0.015, 0),
+	cv::Point3f(-0.18, 0.015, 0),
+	cv::Point3f(-0.18, 0.14, 0),
+	cv::Point3f(-0.305, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(18);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.6, 0.015, 0),
-	cv::Point3f(0.725, 0.015, 0),
-	cv::Point3f(0.6, 0.14, 0),
-	cv::Point3f(0.725, 0.14, 0)
+	cv::Point3f(-0.15, 0.015, 0),
+	cv::Point3f(-0.025, 0.015, 0),
+	cv::Point3f(-0.025, 0.14, 0),
+	cv::Point3f(-0.15, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(19);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.755, 0.015, 0),
-	cv::Point3f(0.88, 0.015, 0),
-	cv::Point3f(0.755, 0.14, 0),
-	cv::Point3f(0.88, 0.14, 0)
+	cv::Point3f(0.005, 0.015, 0),
+	cv::Point3f(0.13, 0.015, 0),
+	cv::Point3f(0.13, 0.14, 0),
+	cv::Point3f(0.005, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(20);
 
 	markerPoints = 
 	{
-	cv::Point3f(0.91, 0.015, 0),
-	cv::Point3f(1.035, 0.015, 0),
-	cv::Point3f(0.91, 0.14, 0),
-	cv::Point3f(1.035, 0.14, 0)
+	cv::Point3f(0.16, 0.015, 0),
+	cv::Point3f(0.285, 0.015, 0),
+	cv::Point3f(0.285, 0.14, 0),
+	cv::Point3f(0.16, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(21);
 
 	markerPoints = 
 	{
-	cv::Point3f(1.065, 0.015, 0),
-	cv::Point3f(1.19, 0.015, 0),
-	cv::Point3f(1.065, 0.14, 0),
-	cv::Point3f(1.19, 0.14, 0)
+	cv::Point3f(0.315, 0.015, 0),
+	cv::Point3f(0.44, 0.015, 0),
+	cv::Point3f(0.44, 0.14, 0),
+	cv::Point3f(0.315, 0.14, 0)
 	};
 	boardCorners.push_back(markerPoints);
 	boardIds.push_back(22);

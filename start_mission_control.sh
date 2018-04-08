fee0eccaf1dbf65bf2128a6a12fe3bacf1746b01
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2018-04-08 13:49:57 -0700

    Update Network Initialization Scripts

--- start_mission_control.sh
+++ start_mission_control.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.4
 
 #roslaunch for mission control goes here
+. devel/setup.bash
+roslaunch tfr_launch mission_control.launch
+

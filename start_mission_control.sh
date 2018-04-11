b3001404287e2c71088443acb75b2be9efd45b20
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2018-04-11 15:48:32 -0700

    modified mission control script

--- start_mission_control.sh
+++ start_mission_control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://192.168.1.3:11311
-export ROS_IP=192.168.1.4
+#export ROS_MASTER_URI=http://192.168.1.3:11311
+#export ROS_IP=192.168.1.4
 
 #roslaunch for mission control goes here
 . devel/setup.bash
